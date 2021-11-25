65d0119067aaaa64b9cf2d97895354af4818c809
Kawan <kawanse0@gmail.com>
2021-11-25 23:30:25 +0100

    fixed almost everything

--- public/script/app.js
+++ public/script/app.js
@@ -1,3 +1,10 @@
-const first = (first) => {
-  alert(first);
-};
+let origin = 0.0;
+
+setInterval(function () {
+  document.querySelector("#distance").innerHTML = Math.ceil(origin) + " M";
+  origin += 1.9;
+}, 100);
+
+function playAudio(url) {
+  new Audio(url).play();
+}
